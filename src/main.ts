75b3f5cd621971e774802f52083ff96036e75edb
dhanashri <dpagar@lenovo.com>
2020-04-01 13:16:13 +0530

    Integrated nedb in electron app with secure db file

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,55 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, ipcMain } from "electron";
 import * as path from "path";
+import os = require('os');
+import Datastore = require('nedb');
+const homeDir = os.homedir();
+import crypto = require('crypto'); // now is in node default module
+
+let algorithm = 'aes-256-cbc'; // you can choose many algorithm from supported openssl
+let secret = 'superSecretKey';
+let key = crypto.createHash('sha256').update(String(secret)).digest('base64').substr(0, 32);
+
+
+// const pathToDb = homeDir + "\work" ;
+
+const db: any = {};
+db['users'] = new Datastore({
+  filename: 'users.db',
+  autoload: true,
+  timestampData: true,
+  corruptAlertThreshold: 0,
+
+  onload: (error) => {
+    console.log("error on load", error);
+  },
+
+  afterSerialization(plaintext) {
+    console.log("Inside afterseriliazation - plaintext is ", plaintext);
+
+    const iv = crypto.randomBytes(16);
+    const aes = crypto.createCipheriv(algorithm, key, iv);
+    let ciphertext = aes.update(plaintext);
+    ciphertext = Buffer.concat([iv, ciphertext, aes.final()]);
+
+    console.log("Inside afterseriliazation - cipherText is ", ciphertext.toString('base64'));
+
+    return ciphertext.toString('base64');
+  },
+
+  beforeDeserialization(ciphertext) {
+    console.log("Inside before deserilization - cipherText is ", ciphertext);
+
+    const ciphertextBytes = Buffer.from(ciphertext, 'base64')
+    const iv = ciphertextBytes.slice(0, 16)
+    const data = ciphertextBytes.slice(16)
+    const aes = crypto.createDecipheriv(algorithm, key, iv)
+    let plaintextBytes = Buffer.from(aes.update(data))
+    plaintextBytes = Buffer.concat([plaintextBytes, aes.final()])
+    let plaintext = plaintextBytes.toString();
+    console.log("Inside before deserilization - plainText is ", plaintext);
+    return plaintext;
+  }
+});
 
 let mainWindow: Electron.BrowserWindow;
 
@@ -41,6 +91,51 @@ app.on("window-all-closed", () => {
     app.quit();
   }
 });
+
+// insert a document
+ipcMain.on('save-user-request', (event, user) => {
+  let newUser = {
+    name: user.name,
+    age: user.age
+  };
+  // console.log("new user - ", newUser);
+
+  db.users.insert(newUser, (err: any, newDoc: any) => {
+    if (err) {
+      event.sender.send('save-user-error', `An error occurred\n${err}`);
+    } else {
+      event.sender.send('save-user-success', newDoc);
+    }
+    console.log("new user ", newUser);
+
+  });
+});
+
+// delete a user recors
+ipcMain.on('delete-user-request', (event, user) => {
+  let usersToDelete: string[];
+  db.users.find({}).sort({ 'createdAt': 1 }).limit(2).exec(function (err: any, docs: any) {
+    usersToDelete = docs.map((user: any) => user._id);
+    console.log("users to delete ", usersToDelete);
+    db.users.remove({ _id: { $in: usersToDelete } }, { multi: true }, function (err: any, numRemoved: any) {
+      console.log("users to delete coming in remove function ", usersToDelete);
+      console.log("number of removed record - ", numRemoved);
+      console.log("error ", err);
+
+    });
+  });
+
+});
+
+// show a first two users
+ipcMain.on('show-user-request', (event, user) => {
+  db.users.find({}).sort({ 'createdAt': 1 }).limit(2).exec(function (err: any, docs: any) {
+    console.log("all users - ", docs);
+  });
+});
+
+
+
 
 app.on("activate", () => {
   // On OS X it"s common to re-create a window in the app when the
--- src/preload.ts
+++ src/preload.ts
@@ -1,14 +1,28 @@
 // All of the Node.js APIs are available in the preload process.
+
+import { ipcRenderer } from "electron";
+
 // It has the same sandbox as a Chrome extension.
 window.addEventListener("DOMContentLoaded", () => {
-  const replaceText = (selector: string, text: string) => {
-    const element = document.getElementById(selector);
-    if (element) {
-      element.innerText = text;
-    }
-  };
 
-  for (const type of ["chrome", "node", "electron"]) {
-    replaceText(`${type}-version`, (process.versions as any)[type]);
-  }
+  // insert a document
+  let btnSaveUser = document.getElementById('save-user');
+  let btnShowUsers = document.getElementById('show-users');
+  let btnDeleteUser = document.getElementById('delete-user');
+
+  btnSaveUser.addEventListener('click', () => {
+    let user: any = {};
+    user.name = (document.getElementById('name') as HTMLInputElement).value;
+    user.age = (document.getElementById('age') as HTMLInputElement).value;
+    ipcRenderer.send('save-user-request', user);
+  });
+
+  btnShowUsers.addEventListener('click', () => {
+    ipcRenderer.send('show-user-request');
+  });
+
+  btnDeleteUser.addEventListener('click', () => {
+    ipcRenderer.send('delete-user-request');
+  });
+
 });
